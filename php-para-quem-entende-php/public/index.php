42adcc8e05df657d5a2edf45dedafdf43a09ff34
Eduardo Henrique <eduardo.henrique.assuncao.ribeiro@gmail.com>
2024-05-21 22:39:52 -0400

    iniciando sessão php para quem entende php

---
+++ php-para-quem-entende-php/public/index.php
@@ -0,0 +1,25 @@
+<?php
+    require_once __DIR__ . "/../app/functions/pages.php";
+?>
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP devclass</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <?php
+            try {
+                require_once load();
+            } catch (Exception $e){
+                echo $e->getMessage();
+            }
+        ?>
+    </div>
+</body>
+</html>
---
+++ php-para-quem-entende-php/public/pages/contato.php
@@ -0,0 +1,25 @@
+<h1>Contato</h1>
+
+<form action="/pages/forms/contato.php" method="post" role="form">
+    <div class="form-group">
+        <label for="name">Nome:</label>
+        <input type="text" class="form-control" name="name" id="name" placeholder="Digite seu nome">
+    </div>
+
+    <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="text" class="form-control" name="email" id="email" placeholder="Digite seu email">
+    </div>
+
+    <div class="form-group">
+        <label for="assunto">Assunto:</label>
+        <input type="text" class="form-control" name="subject" id="assunto" placeholder="Digite o assunto">
+    </div>
+
+    <div class="form-group">
+        <label for="msg">Mensagem:</label>
+        <textarea name="message" id="" cols="120" rows="5" name="message" id="msg" class="form-control"></textarea>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Enviar</button>
+</form>
